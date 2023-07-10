6a26bc627aba1ff24d6469409bbbe7b284903726
Pranay Kumar <pranaykumarmolleti@gmail.com>
2023-07-10 16:45:03 +0530

    1:35

--- src/Home.js
+++ src/Home.js
@@ -11,11 +11,48 @@ function Home() {
 					alt=""
 				/>
 				<div className="home_row">
+					<Product
+						title="Valorant Points"
+						image="https://images-eu.ssl-images-amazon.com/images/I/61Iljj9cOOL._AC_UL450_SR450,320_.jpg "
+						price={20.99}
+						rating={5}
+					/>
+					<Product
+						title="Steam"
+						image="https://images-eu.ssl-images-amazon.com/images/I/61NFS57VEKL._AC_UL450_SR450,320_.jpg"
+						rating={3}
+						price={20.99}
+					/>
+					<Product
+						title="Sony Headset"
+						image="https://images-eu.ssl-images-amazon.com/images/I/41zqAOpwu7L._AC_UL450_SR450,320_.jpg"
+						rating={4}
+						price={10.99}
+					/>
+				</div>
 
-				<Product />
-				<Product />
-                </div>
-
+				<div className="home_row">
+					<Product
+						title="Asus Chromebook"
+						image="https://images-eu.ssl-images-amazon.com/images/I/41XuMF92kzL._AC_UL450_SR450,320_.jpg"
+						price={50.99}
+						rating={4}
+					/>
+					<Product
+						title="beat XP unbound NEO"
+						image="https://images-eu.ssl-images-amazon.com/images/I/61xQOD1ZPUL._AC_UL450_SR450,320_.jpg"
+						price={10}
+						rating={2}
+					/>
+				</div>
+				<div className="home_row">
+					<Product
+						title="boat immortal"
+						image="https://m.media-amazon.com/images/I/41w2pkIRKxL._SX300_SY300_QL70_FMwebp_.jpg"
+						price={5}
+						rating={1}
+					/>
+				</div>
 			</div>
 		</div>
 	);
--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,20 @@
 import React from "react";
 import "./Product.css";
-function Product() {
+function Product({id,title,image,price,rating}) {
 	return (
 		<div className="product">
 			<div className="product_info">
-				<p>12345</p>
+				<p>{title}</p>
 				<p className="product_price">
 					<small>$</small>
-					<strong>1999</strong>
+					<strong>{price}</strong>
 				</p>
 				<div className="product_rating">
-					<p>⭐</p>
-					<p>⭐</p>
-					<p>⭐</p>
+					{Array(rating).fill().map(() => (<p>⭐</p>))}
 				</div>
 			</div>
 			<img
-				src="https://m.media-amazon.com/images/I/41Pn3umtB8L._SR215,215_.jpg"
+				src= {image}
 				alt=""
 			/>
 
